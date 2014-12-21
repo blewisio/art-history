cc8939ceea1151f9a0e778e4111f9fb11763abb1
unknown <brent.lewis08@gmail.com>
2014-12-21 11:36:08 -0600

    gulping sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,13 @@
 'use strict';
 
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat')
-    livereload = require('gulp-livereload'),
-    node;
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-// var cssSrc = ['static/normalize.css', 'static/font-awesome.min.css', 'static/foundation/css/foundation.min.css', 'static/site.css'];
-// var minifyCss = require('gulp-minify-css');
-// gulp.task('styles', function () {
-//     return gulp.src(cssSrc)
-//         .pipe(concat('styles.css'))
-//         .pipe(minifyCss())
-//         .pipe(rename({ suffix: '.min'}))
-//         .pipe(gulp.dest('static/'))
-//         .pipe(livereload(server));
-// });
-
-var serverSrc = 'server.js';
-gulp.task('server', function () {
-    console.log('Restarting server...');
-    if (node) node.kill();
-    node = spawn('node', [serverSrc], {stdio: 'inherit'});
-    console.log('Server restarted.');
-    node.on('close', function (code) {
-        if (code === 8) console.log('err'); // ???
-    });
+var cssSrc = ['art.scss'];
+gulp.task('styles', function () {
+    return gulp.src(cssSrc)
+        .pipe(sass())
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['server'], function () {
-    server.listen(35279, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-
-        // gulp.watch(jadeSrc, ['templates']);
-        // gulp.watch(cssSrc, ['styles']);
-        gulp.watch(serverSrc, ['server']);
-        process.on('exit', function () {
-            if (node) node.kill();
-        });
-    });
-});
+gulp.task('default', ['styles']);
